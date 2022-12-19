bba26b497cfe9133f5f50693f153d16d29357f2b
Mathew Kane <berkough@gmail.com>
2022-12-18 23:26:24 -0800

    first commit

---
+++ game.js
@@ -0,0 +1,63 @@
+const config = {
+  type: Phaser.AUTO,
+  width: 1280,
+  height: 720,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      fps: 60,
+      gravity: {x:0}
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+let player;
+
+const game = new Phaser.Game(config);
+
+function preload(){
+  this.load.image('ship','assets/Starship_1.gif');
+  this.load.image('bg','assets/bg.png');
+}
+
+function create(){
+  this.add.image(640,360,'bg');
+  player = this.physics.add.image(640,360,'ship');
+  player.setDamping(true);
+  player.setDrag(0.99);
+  player.setMaxVelocity(200);
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+}
+
+function update(){
+  console.log(player.rotation);
+
+  if (cursors.up.isDown)
+  {
+      this.physics.velocityFromRotation(player.rotation, 200, player.body.acceleration);
+  }
+  else
+  {
+      player.setAcceleration(0);
+  }
+
+  if (cursors.left.isDown)
+  {
+      player.setAngularVelocity(-150);
+  }
+  else if (cursors.right.isDown)
+  {
+      player.setAngularVelocity(150);
+  }
+  else
+  {
+      player.setAngularVelocity(0);
+  }
+}
